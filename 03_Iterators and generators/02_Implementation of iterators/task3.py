3bc1d329be29be736227a6dd1b39981c4021efde
Pasik <parasatto@gmail.com>
2023-03-22 21:28:52 +0600

    implemetion of iterators

---
+++ 03_Iterators and generators/02_Implementation of iterators/task3.py
@@ -0,0 +1,25 @@
+import random
+
+
+class SumsOfLastTwo:
+
+    def __init__(self, count):
+        self.last = 0
+        self.end = count
+        self.start = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.start += 1
+        if self.start > self.end:
+            raise StopIteration
+        self.last += random.random()
+        return self.last
+
+
+counter = SumsOfLastTwo(5)
+for i in counter:
+    print(i)
+
