345a67eb36780e5366705dcebb9321f890b298d5
Pasik <parasatto@gmail.com>
2023-04-01 22:20:34 +0600

    decorators with arg

---
+++ 06_Decorators Advanced level/02_Decorators with arguments/task1.py
@@ -0,0 +1,18 @@
+def do_nice(n):
+    def wrap_fun(func):
+        """""декоратор do_twice, который дважды вызывает декорируемую функцию"""
+
+        def wrapped_func(*args, **kwargs):
+            for _ in range(n):
+                func(*args, **kwargs)
+            return
+        return wrapped_func
+    return wrap_fun
+
+
+@do_nice(5)
+def greeting(name: str) -> None:
+    print('Привет, {name}'.format(name=name))
+
+
+greeting('Adam')
---
+++ 06_Decorators Advanced level/02_Decorators with arguments/task2.py
@@ -0,0 +1,24 @@
+from time import sleep
+
+
+def slow_it_now(_func=None, *, n=1):
+    def slowdown_ns(func):
+        def wrapper(*args, **kwargs):
+            sleep(n)
+            result = func(*args, **kwargs)
+            return result
+        return wrapper
+
+    if _func is None:
+        return slowdown_ns
+    else:
+        return slowdown_ns(_func)
+
+
+@slow_it_now
+def test() -> None:
+    """Проверка декоратора и вывод прстого сообщения return:"""
+    print('Here`s happening somthing')
+
+
+test()
